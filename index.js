6462ec34a0a03b8ec4993c18448aa68a34bed8d9
Javier Jiménez <javier.jimenez.villarreal@gmail.com>
2020-11-10 17:16:52 +0100

    feat: richer extract

--- index.js
+++ index.js
@@ -7,17 +7,56 @@ const org = "abacum-io";
 const gifRegexp = /\!\[\]\((.*gif)\)/gm;
 
 const getRepos = async () => octokit.repos.listForOrg({ org });
-const getPRs = async (repo) =>
-  octokit.pulls.list({ owner: org, repo, state: "all" });
 
-const gifsFromPr = (pr) => {
-  const match = gifRegexp.exec(pr.body);
-  return match ? match[1] : null;
+const getPRsPage = async (repo, page) => {
+  return octokit.pulls.list({
+    owner: org,
+    repo,
+    state: "all",
+    sort: "created",
+    per_page: 50,
+    page,
+  });
 };
+
+const getPRs = async (repo, page = 0, prs = []) => {
+  const { data } = await getPRsPage(repo, page);
+
+  if (data.length === 0) {
+    return prs;
+  }
+  return await getPRs(repo, page + 1, prs.concat(data));
+};
+
+const extractGif = (body) => {
+  const match = gifRegexp.exec(body);
+  return !!match ? match[1] : null;
+};
+
+const infoFromPr = ({ user, body, html_url, title, created_at }) => {
+  const gif = extractGif(body);
+  return {
+    gif,
+    user: user.login,
+    avatar: user.avatar_url,
+    html_url,
+    created_at,
+    title,
+  };
+};
+
+const withGifAndGivenMonth = (lastMonth, { gif, created_at }) => {
+  const month = new Date(Date.parse(created_at)).getMonth();
+  return gif && month === lastMonth;
+};
+
 const gifsFromRepo = async (repo) => {
-  const { data } = await getPRs(repo.name);
-  const gifs = data.map((pr) => gifsFromPr(pr));
-  return gifs.filter((obj) => obj); // cleanup nulls
+  const lastMonth = new Date().getMonth();
+  const prs = await getPRs(repo.name);
+  const mappedPrs = prs.map(infoFromPr);
+  const gifs = mappedPrs.filter((pr) => withGifAndGivenMonth(lastMonth, pr));
+
+  return gifs; // cleanup nulls
 };
 
 const gifsFromRepos = async (repos) =>
@@ -29,4 +68,6 @@ const getGifs = async () => {
   return gifsList.flat();
 };
 
-getGifs().then((gifs) => console.log("finished", gifs));
+getGifs()
+  .then((gifs) => console.log("finished", gifs))
+  .catch(console.log);
