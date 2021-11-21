02e485dcf38f05e0f04c94acb87b8a3f7f4bad6c
Javier Jiménez <javier.jimenez.villarreal@gmail.com>
2021-11-21 19:47:25 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const github = require("./github");
 const typeform = require("./typeform");
-const mailer = require("./mailer");
 
-github.getGifs().then(typeform.createForm).then(mailer.notifyParticipants);
+
+github.getGifs().then(typeform.createForm);
